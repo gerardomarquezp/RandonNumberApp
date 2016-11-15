b8a9f74040281d454ca5635d9cec927c49ea2490
Gerardo Marquez <Gerardo Marquez>
2016-11-15 11:39:50 -0700

    Se inicia funcionalidad para toast en button (INCOMPLETO)

--- app/src/main/java/com/example/gerardo/randomnumberapp/MainActivity.java
+++ app/src/main/java/com/example/gerardo/randomnumberapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.gerardo.randomnumberapp;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -14,15 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Random rand = new Random();
-        int n = rand.nextInt(20);
 
-        Context context = getApplicationContext();
-        String number = n + "number";
-        int duration = Toast.LENGTH_LONG;
+        final Button btnMostrar= (Button) findViewById(R.id.btnmostrar);
 
-        Toast toast = Toast.makeText(context, number, duration);
-        toast.show();
+        btnMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rand = new Random();
+                int n = rand.nextInt(20);
+                Context context = getApplicationContext();
+                String number = n + "number";
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, number, duration);
+                toast.show();
+            }
+        });
+
+
 
     }
 }
